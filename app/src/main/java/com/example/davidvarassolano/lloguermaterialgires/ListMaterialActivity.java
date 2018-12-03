9a227a6e240004494819b6c0754a7fdda02bd4d6
David Varas <dvarasolano@gmail.com>
2018-12-03 13:19:57 +0100

    Añado actividad ListMaterialActivity

---
+++ app/src/main/java/com/example/davidvarassolano/lloguermaterialgires/ListMaterialActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidvarassolano.lloguermaterialgires;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListMaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_material);
+    }
+}
